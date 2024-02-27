46257b4a7c16e5c4ecbd5d9cf5e3d49366ceb8c2
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-02-27 14:44:52 +0500

    Building Input Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Navbar from "./components/Navbar";
 import {FiSearch} from "react-icons/fi"
+import {AiFillPlusCircle} from "react-icons/ai"
 
 export default function App() {
   return (
    <div className=" max-auto max-w-[370px]  m-4" >
     <Navbar />
-    <div className="flex relative flex-grow items-center">
+    <div className="flex relative flex-grow items-center gap-2">
     <FiSearch className="text-white text-3xl absolute ml-2 " />
       <input type="text" placeholder="Search Contacts" className=" text-white pl-10 flex-grow bg-transparent h-10 border-white rounded-md border " />
+      <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />   
     </div>
-   </div>
+  </div>
   )
 }
